7966d29e453a0548ded2f9b2ee91d9e9837732b8
HauVal <valentin.hauspie@laposte.net>
2025-01-21 19:33:21 +0100

    front inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,16 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
@@ -22,25 +18,20 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 'label' => 'Nom utilisateur',
-                'attr' => ['placeholder' => 'Entrez votre nom utilisateur']
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Adresse mail',
-                'attr' => ['placeholder' => 'Entrez votre adresse mail']
             ])
             ->add('deliveryAddress', TextType::class, [
                 'label' => 'Adresse de livraison',
-                'attr' => ['placeholder' => 'Entrez votre adresse de livraison']
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => [
                     'label' => 'Mot de passe',
-                    'attr' => ['placeholder' => 'Entrez un mot de passe']
                 ],
                 'second_options' => [
                     'label' => 'Confirmation du mot de passe',
-                    'attr' => ['placeholder' => 'Confirmez votre mot de passe']
                 ],
                 'invalid_message' => 'Les mots de passe doivent correspondre.',
             ]);
